04b09d46e2a2c31de37f1a83f1390548b8dced35
qiuchengxuan <qiuchengxuan@gmail.com>
2026-01-24 18:14:56 +0800

    bump submodules

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -85,8 +85,16 @@
 (after! centaur-tabs
   (defun centaur-tabs-buffer-groups () "" (list "Default")))
 
+(setq gptel-directives
+    '((default . "回复应简明扼要")
+      (programming . "你是一位专业的编程助手，你的职责是提供高质量的代码解决方案、重构和解释")
+      (writing . "你是一位专业的写作助手，回复应简明扼要")
+      (chat . "回复应简明扼要")
+      (rewrite . "你是一位专业的编程助手，需要改写这段代码")))
 (setq gptel-model 'deepseek-chat
-      gptel-backend (gptel-make-deepseek "DeepSeek" :stream t :key gptel-api-key))
+      gptel-backend (gptel-make-deepseek "DeepSeek" :key gptel-api-key :stream t))
+(gptel-make-gemini "Gemini" :key gptel-api-key :stream t)
+(gptel-make-anthropic "Claude" :models '(claude-sonnet-4-5-20250929) :key gptel-api-key :stream t)
 
 (setq treesit-language-source-alist
       '((c "https://github.com/tree-sitter/tree-sitter-c" "v0.20.8")
@@ -98,8 +106,7 @@
 
 (setq auto-save-default nil)
 
-(add-hook 'c-mode-hook (lambda () (setq tab-width 8)))
-(add-hook 'c-mode-common-hook (lambda () (setq tab-width 8)))
+(setq c-default-style "linux")
 (add-hook 'go-mode-hook (lambda () (setq tab-width 8)))
 (add-hook 'js-mode-hook (lambda () (setq js-switch-indent-offset 4)))
 (setq-hook! 'sh-mode-hook +format-with :none)
